678f3df154efa0cbe556d9556c76a9bd8d8e276d
caliopecarat <ddygetins@gmail.com>
2025-11-29 16:12:09 -0600

    Update picadita_jarocha.sql

--- picadita_jarocha.sql
+++ picadita_jarocha.sql
@@ -1,1 +1,6 @@
+/*Entidades independientes
+Restaurante, sucursal, sucursal, rol, area_impresion
+categoria_modificador, descuento, promocion, area_cocina, metodo_pago**/
 
+INSERT INTO restaurante (nombre, rfc)
+VALUES ('La Picadita Jarocha', 
